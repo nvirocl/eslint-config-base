a1c39d6a020de6117ae81c7336e110b350032bf3
Edward Moreno <edward.moreno@ing.uchile.cl>
2021-12-10 12:48:18 -0300

    Add file for check lint

---
+++ test.js
@@ -0,0 +1,30 @@
+'use strict';
+
+
+let { c, d } = {};
+d = 3;
+
+const f1 = arg => arg;
+
+
+// con error
+if (x == 42) { }
+
+if ('' == text) { }
+
+if (obj.getStuff() != undefined) { }
+
+let b = 4; // let sin asignar
+
+const q = () => { return; };
+
+const r = () => {return;};
+
+(arg) => { arg; };
+
+arg => { return arg; };
+
+
+arg => {
+  return arg;
+};
